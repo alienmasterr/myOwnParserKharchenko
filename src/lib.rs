d1dc492e28cd11dc3db08d7d0c23f83a3541903e
alina <alina230605@gmail.com>
2025-11-12 14:19:23 +0200

    decomposed

--- src/lib.rs
+++ src/lib.rs
@@ -28,15 +28,25 @@ fn checkBalancedBrackets(s: &str) -> bool {
     balance == 0
 }
 
-pub fn parseExpression(s: &str) -> Result<ParseResult, ParseError> {
-
-    if s.starts_with('(') && s.ends_with(')') {
-        return parseExpression(&s[1..s.len()-1]);
+    //rule 0: handle (x)
+    // if s.starts_with('(') && s.ends_with(')') {
+    //     return parseExpression(&s[1..s.len()-1]);
+    // }
+fn remove_outer_brackets(s: &str) -> Option<&str> {
+    if s.starts_with('(') && s.ends_with(')'){
+        return Some(&s[1..s.len() - 1]);
+    }else{
+        return None;
     }
+}
 
-    if (s.contains("+(-") || s.contains("-(-") || s.contains("*(-")|| s.contains("/(-")) && s.ends_with(')') {
-
-        let mut new_s = s.replace("+(-", "-");
+//rule 1 : handle +(-x), -(-x), *(-x), /(-x)
+fn has_minus(s: &str) -> bool {
+    return (s.contains("+(-") || s.contains("-(-") || s.contains("*(-") || s.contains("/(-")) && s.ends_with(')');
+}
+//rule 1 : handle +(-x), -(-x), *(-x), /(-x)
+fn simplify_minus(s: &str) -> String {
+    let mut new_s = s.replace("+(-", "-");
         new_s = new_s.replace("-(-", "+");
         new_s = new_s.replace("*(-", "*-");
         new_s = new_s.replace("/(-", "/-");
@@ -45,26 +55,30 @@ pub fn parseExpression(s: &str) -> Result<ParseResult, ParseError> {
             new_s.pop();
         }
 
-        return parseExpression(&new_s);
-    }
+    return new_s;
+}
 
-    if s.starts_with("-(") && s.ends_with(')') {
-        
-        let inner = &s[2..s.len() - 1];
-        let inner_result = parseExpression(inner)?;
+//rule 1: handle -(-x)
+fn has_minus_at_start(s: &str) -> bool {
+    return s.starts_with("-(") && s.ends_with(')');
+}
+
+//rule 1: handle -(-x)
+fn handle_minus_at_start(s: &str) -> Result<ParseResult, ParseError> {
+    let inner = &s[2..s.len() - 1];
+    let inner_result = parseExpression(inner)?;
     
         return Ok(ParseResult {
             result: -inner_result.result,
             operands: inner_result.operands,
             operators: inner_result.operators,
         });
-    }
+}
 
-    if s.trim().is_empty() || !checkBalancedBrackets(s) {
+// rule 2: handle +, -, *, /
+fn pasre_simple_expression(s: &str) -> Result<ParseResult, ParseError> {
 
-        return Err(ParseError::InvalidExpression);
-
-    } else if s.contains('+'){
+    if s.contains('+'){
 
         let parts: Vec<&str> = s.split('+').collect();
         if parts.len() != 2 {
@@ -133,8 +147,28 @@ pub fn parseExpression(s: &str) -> Result<ParseResult, ParseError> {
     })
 
     } else {
-
     Err(ParseError::InvalidExpression)
+    }
+}
 
+// the function to parse expression
+pub fn parseExpression(s: &str) -> Result<ParseResult, ParseError> {
+    if let Some(inner) = remove_outer_brackets(s){
+        return parseExpression(inner);
+    }
+
+    if has_minus(s) {
+        let simplified = simplify_minus(s);
+        return parseExpression(&simplified);
+    }
+
+    if has_minus_at_start(s) {
+        return handle_minus_at_start(s);
+    }
+
+    if s.trim().is_empty() || !checkBalancedBrackets(s) {
+        return Err(ParseError::InvalidExpression);
+    } else{
+        return pasre_simple_expression(s);
     }
 }
